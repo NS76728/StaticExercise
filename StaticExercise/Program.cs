dab8e4d1b5ed19ddc778ff699b0cd43f7906ce3d
Nick Stockton <phoenix76728@gmail.com>
2021-09-01 14:35:24 -0400

    initial commit

---
+++ StaticExercise/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace StaticExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What temperature do you want to convert?");
+            string input = Console.ReadLine();
+
+            if (input== "fahrenheit")
+            {
+                Console.WriteLine("What is the fahrenheit temperature you want to convert?");
+                double use = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine(TempConverter.FahrenheitToCelsius(use));
+            }
+            else if (input == "celsius")
+            {
+                Console.WriteLine("What is the celsius temperature you want to convert?");
+                double use = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine(TempConverter.CelsiusToFahrenheit(use));
+            }
+        }
+    }
+}
---
+++ StaticExercise/TempConverter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StaticExercise
+{
+    public static class TempConverter
+    {
+
+        public static double FahrenheitToCelsius(double temp)
+        {
+            double Ctemp = (temp - 32) * (5 / 9);
+
+            return Ctemp;
+
+        }
+        public static double CelsiusToFahrenheit(double temp)
+        {
+
+            double Ftemp = ((temp * 9 )/5) + 32;
+            return Ftemp;
+
+        }
+
+
+
+
+
+
+    }
+}
